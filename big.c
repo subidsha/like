829c8d4f38f6daefa6c398343a54025fd1fab0ac
subidsha <31504698+subidsha@users.noreply.github.com>
2017-09-22 21:25:47 +0530

    Create big.c

---
+++ big.c
@@ -0,0 +1,17 @@
+#include<stdioh>
+int a,bc;
+printf("enter the number",a,b,c);
+scanf("%d %d %d",a,b,c);
+if(a>b)
+{
+printf("a is big",a);
+}
+elseif(b>c)
+{
+printf("b is big",b);
+}
+else
+{
+printf("c is big",c);
+}
+}
